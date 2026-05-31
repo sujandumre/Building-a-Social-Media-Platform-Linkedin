7807730f7ac493c9c919906e26f5d7f091ba7632
sujan dumre <dumresujan12@gmail.com>
2026-05-31 22:58:25 +0545

    fix bugs

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -10,7 +10,7 @@ const router = Router();
 router.post("/login", login);
 router.post("/register", register);
 
-import { storage } from "../config/cloudinary.js";
+
 const upload = multer({ storage });
 
 
