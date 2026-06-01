7b58132afba603726e9d32a9625de76e3d8ca269
sujan dumre <dumresujan12@gmail.com>
2026-06-01 21:56:11 +0545

    fix media

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -7,11 +7,21 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// export const storage = new CloudinaryStorage({
+//   cloudinary,
+//   params: {
+//     folder: "linkedin-clone",
+//     allowed_formats: ["jpg", "png", "jpeg"],
+//   },
+// });
+
+
 export const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "linkedin-clone",
     allowed_formats: ["jpg", "png", "jpeg"],
+    resource_type: "auto",
   },
 });
 
